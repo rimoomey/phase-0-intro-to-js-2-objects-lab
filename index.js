fb78fe8b3b2b9982dbb8063d8fe4b9172e39641b
Rimon Moomey <rimon.moomey@gmail.com>
2022-10-12 13:57:41 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+const employee = {
+  name: "Bob",
+  streetAddress: "101 Mangolia Drive",
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return {
+    ...employee,
+    [key]: value,
+  };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  const copyEmployee = { ...employee };
+  delete copyEmployee[key];
+  return copyEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
